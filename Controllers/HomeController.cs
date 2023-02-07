04c588a0831a283aa02935c61a9d9800779beebe
autdel <114094641+autdel@users.noreply.github.com>
2023-02-06 23:53:03 -0700

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission05_autdel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission05_autdel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calculator(CalculatorModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/CalculatorModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission05_autdel.Models
+{
+    public class CalculatorModel
+    {
+        // Model for calculations - 6 different fields that are all required
+        [Required]
+        [Range(0, 100, ErrorMessage = "Enter number between 0 to 100")]
+        public int assignments { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Enter number between 0 to 100")]
+        public int groupProjects { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Enter number between 0 to 100")]
+        public int quizzes { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Enter number between 0 to 100")]
+        public int midtermExam { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Enter number between 0 to 100")]
+        public int finalExam { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Enter number between 0 to 100")]
+        public int intex { get; set; }
+    }
+}
